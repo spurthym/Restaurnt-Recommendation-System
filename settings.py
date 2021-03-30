d3ed5d00250187e404cf749daf479f4fad1cf5e1
spurthym <spurthymu99@gmail.com>
2021-03-30 15:12:00 +0530

    Settings file

---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings:
+    def __init__(self):
+        pass
+
+    DATASET_FILE = '/Users/spurthy/Desktop/final_project_nm/dataset/tempe_review_1600.json'
+    MONGO_CONNECTION_STRING = "mongodb://localhost:27017/"
+    # REVIEWS_DATABASE = "Dataset_Challenge_Reviews" # original, trained
+
+    TAGS_DATABASE = "Tags"
+    REVIEWS_COLLECTION = "inv"
+    CORPUS_COLLECTION = "Corpus"
+    USER_DATABASE = "usr"
+    USER_COLLECTION = "u"
+    BUSINESS_COLLECTION = "b"
+    USER_STOP="su"
+    BUSINESS_STOP="sb"
+    USER_PROFILE="up"
+    BUSINESS_PROFILE="ub"
